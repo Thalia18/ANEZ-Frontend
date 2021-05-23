b50e501a80013bb7bec547cf7ab3614dee234ff2
Thalía Zapata <thaliazapata@Thalias-MacBook-Pro.local>
2021-05-22 19:49:37 -0500

    prueba safari v7

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import 'regenerator-runtime/runtime';
 import App from './App';
 import { persistor, store } from './configStore';
 import './index.css';
