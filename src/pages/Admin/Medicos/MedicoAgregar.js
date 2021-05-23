d3d6e8248c30137b5eb96c56358a736fcab61921
Thalía Zapata <thaliazapata@Thalias-MacBook-Pro.local>
2021-05-22 21:17:37 -0500

    prueba safari v10

--- src/pages/Admin/Medicos/MedicoAgregar.js
+++ src/pages/Admin/Medicos/MedicoAgregar.js
@@ -276,12 +276,13 @@ class MedicoAgregar extends Component {
   render() {
     if (this.state.loading) return <Loader />;
     if (this.state.error) return <Error />;
+    console.log(this.state.consultorios);
     return (
       <React.Fragment>
         <Layout activeKeyP="5">
           <Navbar />
 
-          {!this.state.sesion && (
+          {/* {!this.state.sesion && (
             <Agregar
               headerC="Nuevo Médico"
               icon="add circle"
@@ -295,7 +296,13 @@ class MedicoAgregar extends Component {
               handleOnChangeEspecialidad={this.handleOnChangeEspecialidad}
               emailCorrect={this.state.emailCorrect}
             />
-          )}
+          )} */}
+          <div>
+            funciona
+            {this.state.consultorios.map((item) => {
+              return <p key={item.key}>{item.text}</p>;
+            })}
+          </div>
           <Sesion open={this.state.sesion} />
         </Layout>
       </React.Fragment>
